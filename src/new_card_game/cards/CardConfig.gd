9126371650271a0e65326b4924e417815be586e8
jjofnwa <jjofnwa@gmail.com>
2021-03-07 13:22:08 -0600

    Finished quick start.  Not loading the Card Front and Back Designs in step 2 properly, lead issues later when inheriting from CardTemplate.tcsn

--- src/new_card_game/cards/CardConfig.gd
+++ src/new_card_game/cards/CardConfig.gd
@@ -14,7 +14,7 @@ extends Reference
 # Properties which are placed as they are in appropriate labels
 const PROPERTIES_STRINGS := ["Type", "Requirements", "Abilities"]
 # Properties which are converted into string using a format defined in setup()
-const PROPERTIES_NUMBERS := ["Cost","Power"]
+const PROPERTIES_NUMBERS := ["Cost","Power","Health"]
 const NUMBER_WITH_LABEL := ["Cost","Power"]
 # Properties provided in a list which are converted into a string for the
 # label text, using the array_join() method
---
+++ src/new_card_game/cards/sets/SetDefinition_Core.gd
@@ -0,0 +1,25 @@
+# This file contains just card definitions. See also `CardConfig.gd`
+
+extends Reference
+
+const SET = "Core Set"
+const CARDS := {
+	"Battle Beast": {
+		"Type": "Creature",
+		"Tags": ["Brutal", "Slow"],
+		"Requirements": "",
+		"Abilities": " ",
+		"Cost": 2,
+		"Power": 3,
+		"Health": 3,
+	},
+	"Beast in Black": {
+		"Type": "Creature",
+		"Tags": ["Fast", "Flanking"],
+		"Requirements": "Cannot be played on first turn",
+		"Abilities": " ",
+		"Cost": 1,
+		"Power": 2,
+		"Health": 1,
+	},
+}
